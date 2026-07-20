b9b2ca45e981e77f551a29177eac3f5f0b64d300
Shen Tang <adatang23@yahoo.com>
2026-07-20 18:26:19 -0400

    Added Qdrant Vector Store configurations using application.properties and docker-compose.yaml, and started the project and Docker Container of Qdrant Vector Store

--- section5/springai/src/test/java/com/eazybytes/springai/SpringAiApplicationTests.java
+++ section5/springai/src/test/java/com/eazybytes/springai/SpringAiApplicationTests.java
@@ -3,7 +3,7 @@ package com.eazybytes.springai;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringAiApplication.class)
 class SpringAiApplicationTests {
 
 	@Test
