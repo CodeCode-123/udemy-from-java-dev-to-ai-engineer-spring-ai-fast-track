962ee892169fcb31b37f278191ee7e3c45562692
Shen Tang <adatang23@yahoo.com>
2026-07-21 15:07:40 -0400

    Added PDF and Chunking to break down big documents for RAG, added web search configuration using Tavily to retrieve information from websites for the latest news, and tested using Postman

--- section5/springai/src/main/java/com/eazybytes/springai/controller/RAGController.java
+++ section5/springai/src/main/java/com/eazybytes/springai/controller/RAGController.java
@@ -20,34 +20,68 @@ import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
 public class RAGController {
 
     private final ChatClient chatClient;
+    private final ChatClient webSearchchatClient;
     private final VectorStore vectorStore;
 
     @Value("classpath:/promptTemplates/systemPromptRandomDataTemplate.st")
     Resource promptTemplate;
 
+    @Value("classpath:/promptTemplates/systemPromptTemplate.st")
+    Resource hrSystemPromptTemplate;
+
     public RAGController(@Qualifier("chatMemoryChatClient") ChatClient chatClient,
+                         @Qualifier("webSearchRAGChatClient") ChatClient webSearchchatClient,
                          VectorStore vectorStore) {
         this.chatClient = chatClient;
+        this.webSearchchatClient = webSearchchatClient;
         this.vectorStore = vectorStore;
     }
 
     @GetMapping("/random/chat")
     public ResponseEntity<String> randomChat(@RequestHeader("username") String username,
                                              @RequestParam("message") String message) {
-        //set up the configuration of searchRequest
-        SearchRequest searchRequest =
-                SearchRequest.builder().query(message).topK(3).similarityThreshold(0.5).build();
-        //search in the vectorStore and generate the similarDocs
-        List<Document> similarDocs = vectorStore.similaritySearch(searchRequest);
-        //fetch the actual text information present in each of the document object collected as a
-        //single string, each document text is going to be separated by using a line separator
-        //this similarContext will be passed as an input into the PromptTemplate
-        String similarContext = similarDocs.stream()
-                .map(Document::getText)
-                .collect(Collectors.joining(System.lineSeparator()));
+//        //set up the configuration of searchRequest
+//        SearchRequest searchRequest =
+//                SearchRequest.builder().query(message).topK(3).similarityThreshold(0.5).build();
+//        //search in the vectorStore and generate the similarDocs
+//        List<Document> similarDocs = vectorStore.similaritySearch(searchRequest);
+//        //fetch the actual text information present in each of the document object collected as a
+//        //single string, each document text is going to be separated by using a line separator
+//        //this similarContext will be passed as an input into the PromptTemplate
+//        String similarContext = similarDocs.stream()
+//                .map(Document::getText)
+//                .collect(Collectors.joining(System.lineSeparator()));
         String answer = chatClient.prompt()
-                .system(promptSystemSpec -> promptSystemSpec.text(promptTemplate)
-                        .param("documents", similarContext))
+//                .system(promptSystemSpec -> promptSystemSpec.text(promptTemplate)
+//                        .param("documents", similarContext))
+                .advisors(a -> a.param(CONVERSATION_ID, username))
+                .user(message)
+                .call().content();
+        return ResponseEntity.ok(answer);
+    }
+
+    @GetMapping("/document/chat")
+    public ResponseEntity<String> documentChat(@RequestHeader("username") String username,
+                                             @RequestParam("message") String message) {
+//        SearchRequest searchRequest =
+//                SearchRequest.builder().query(message).topK(3).similarityThreshold(0.5).build();
+//        List<Document> similarDocs = vectorStore.similaritySearch(searchRequest);
+//        String similarContext = similarDocs.stream()
+//                .map(Document::getText)
+//                .collect(Collectors.joining(System.lineSeparator()));
+        String answer = chatClient.prompt()
+//                .system(promptSystemSpec -> promptSystemSpec.text(hrSystemPromptTemplate)
+//                        .param("documents", similarContext))
+                .advisors(a -> a.param(CONVERSATION_ID, username))
+                .user(message)
+                .call().content();
+        return ResponseEntity.ok(answer);
+    }
+
+    @GetMapping("/web-search/chat")
+    public ResponseEntity<String> webSearchChat(@RequestHeader("username") String username,
+                                                @RequestParam("message") String message) {
+        String answer = webSearchchatClient.prompt()
                 .advisors(a -> a.param(CONVERSATION_ID, username))
                 .user(message)
                 .call().content();
---
+++ section5/springai/src/main/java/com/eazybytes/springai/rag/HRPolicyLoader.java
@@ -0,0 +1,34 @@
+package com.eazybytes.springai.rag;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class HRPolicyLoader {
+    private final VectorStore vectoStore;
+
+    @Value("classpath:Eazybytes_HR_Policies.pdf")
+    Resource policyFile;
+
+    public HRPolicyLoader(VectorStore vectoStore) {
+        this.vectoStore = vectoStore;
+    }
+
+    @PostConstruct
+    public void loadPDF() {
+        TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(policyFile);
+        List<Document> docs = tikaDocumentReader.get();
+        TextSplitter textSplitter =
+                TokenTextSplitter.builder().withChunkSize(100).withMaxNumChunks(400).build();
+        vectoStore.add(textSplitter.split(docs));
+    }
+}
