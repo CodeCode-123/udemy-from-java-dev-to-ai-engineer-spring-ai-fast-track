5e11f25f1198ffe305c86922c0c901dbf5017718
Shen Tang <adatang23@yahoo.com>
2026-07-28 15:26:16 -0400

    Initial commit

---
+++ section9/springai/src/main/java/com/eazybytes/springai/config/ChatClientConfig.java
@@ -0,0 +1,31 @@
+package com.eazybytes.springai.config;
+
+import com.eazybytes.springai.advisors.TokenUsageAuditAdvisor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class ChatClientConfig {
+
+    @Bean("chatClient")
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+        var options = OpenAiChatOptions.builder().model("gpt-5.4-mini").temperature(0.8);
+        return chatClientBuilder
+                .defaultOptions(options)
+                .defaultSystem("""
+                    You are an internal HR assistant. Your role is to help\\s
+                    employees with questions related to HR policies, such as\\s
+                    leave policies, working hours, benefits, and code of conduct.
+                    If a user asks for help with anything outside of these topics,\\s
+                    kindly inform them that you can only assist with queries related to\\s
+                    HR policies.
+                    """)
+                .defaultUser("How can you help me ?")
+                .build();
+    }
+}
---
+++ section9/springai/src/test/java/com/eazybytes/springai/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SpringAiApplication.class)
+class SpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
